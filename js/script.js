c61c3815aa88768bb0ae5eecd3771e9a6bc88272
NataKr <finka1983@gmail.com>
2018-01-07 19:43:21 +0200

    added functionality

--- js/script.js
+++ js/script.js
@@ -2,23 +2,21 @@ var Buzz={};
 
 Buzz.init=function(){
   document.getElementById("btn").addEventListener("click", Buzz.showVideo);
-  //document.getElementById("second-button").addEventListener("click", Buzz.showDiary);
 }
 
 Buzz.showVideo=function(){
   document.getElementById("btn-container").style.display="none";
   document.getElementById("video-container").style.display="block";
   var video = document.getElementById("myvideo");
+  var h=window.innerHeight;
+  var w=window.innerWidth;
+  video.style.height=h+"px";
+  video.style.width=w+"px";
+  console.log(h);
   video.play();
   setTimeout(function(){
     document.getElementById("second-button").style.display="inline-block";
-  }, 5000);
-}
-
-Buzz.showDiary=function(){
-  console.log("I am in modal");
-  $("#myModal").show();
-  console.log("I am in modal");
+  }, 65000);
 }
 
 Buzz.init();
